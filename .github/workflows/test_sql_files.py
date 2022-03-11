9061ea961b4d62f2b33f4315b1fcbe5f3f6962e6
Abhiram98 <bellur.abhiram@gmail.com>
2022-03-11 17:58:30 +0530

    Parse changed files

--- .github/workflows/test_sql_files.py
+++ .github/workflows/test_sql_files.py
@@ -1,8 +1,19 @@
 import sys
+import sqlfulff
 print("Testing files ->", sys.argv)
 
-files_changes = sys.argv
-sql_files = list(filter(lambda x: x.split('.')[-1] == 'sql', files_changes))
+file = sys.argv[1]
+with open(file) as f:
+	changed_files = f.read().split()
+
+print(f"Changed files {changed_files}")
+sql_files = lsit(filter(lambda x: x.split('.')[-1] == 'sql', changed_files))
 print("SQL Files changed ->", sql_files)
+
+for f in sql_files:
+	print(f"Parsing {f}")
+	with open(f) as fp:
+		query = fp.read()
+		sqlfulff.parse(query, dialect = 'redshift')
 
 print("Test complete.")
