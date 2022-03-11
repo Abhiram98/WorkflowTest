e6344f118a6bf66605fa1d1ea184abb190d22d77
Abhiram98 <bellur.abhiram@gmail.com>
2022-03-11 18:11:56 +0530

    Update test_sql_files.py
    Exit with sys code 1 if failed

--- .github/workflows/test_sql_files.py
+++ .github/workflows/test_sql_files.py
@@ -11,6 +11,8 @@ print(f"Changed files {changed_files}")
 sql_files = list(filter(lambda x: x.split('.')[-1] == 'sql', changed_files))
 print("SQL Files changed ->", sql_files)
 
+failed = False
+
 for f in sql_files:
 	print(f"Parsing {f}")
 	with open(f) as fp:
@@ -20,5 +22,9 @@ for f in sql_files:
 		except Exception as e:
 			print(f"Failed to parse {f}")
 			print(f"error: {str(e)}")
+			failed = True
+
 
 print("Test complete.")
+print("Failed? {failed}")
+sys.exit(1)
