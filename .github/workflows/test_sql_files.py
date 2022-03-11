d325f3a4263142719d0bfd07a24b2c41e9461e8d
Abhiram98 <bellur.abhiram@gmail.com>
2022-03-11 18:04:17 +0530

    Update test_sql_files.py

--- .github/workflows/test_sql_files.py
+++ .github/workflows/test_sql_files.py
@@ -3,11 +3,12 @@ import sqlfluff
 print("Testing files ->", sys.argv)
 
 file = sys.argv[1]
+print(f"Content File {file}")
 with open(file) as f:
 	changed_files = f.read().split()
 
 print(f"Changed files {changed_files}")
-sql_files = lsit(filter(lambda x: x.split('.')[-1] == 'sql', changed_files))
+sql_files = list(filter(lambda x: x.split('.')[-1] == 'sql', changed_files))
 print("SQL Files changed ->", sql_files)
 
 for f in sql_files:
